326480a6ef6fe4226ac33c28037ea41186c5e859
harsh <harsh710pat@gmail.com>
2020-08-25 01:27:48 +0530

    Completed the backend initializing front-end

--- routes/apis/auth.js
+++ routes/apis/auth.js
@@ -43,6 +43,8 @@ router.post(
       let user = await User.findOne({ email });
       if (!user) {
         return res
+
+        
           .status(400)
           .json({ errors: [{ msg: "Invalid Credentials" }] });
       }
