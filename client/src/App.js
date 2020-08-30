8664cf3789a0c690122e4a5c7080ad5fc7f91a88
harsh <harsh710pat@gmail.com>
2020-08-30 14:09:07 +0530

    Completed Alerts Using Redux

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import Alert from "./components/layout/Alert";
 
 //Redux Stuff
 
@@ -18,6 +19,7 @@ const App = () => (
         <Navbar />
         <Route exact path="/" component={Landing} />
         <section className="container">
+          <Alert />
           <Switch>
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
